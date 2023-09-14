0cc05ea4ca33fb491691eb06cd9fadadf9b5741d
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-14 11:37:58 +0200

    done

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from math import pi
 
-circle_area = None # Kreisfläche
-
-square_area = None # Quadratfläche
-
-rectangle_area = None # Rechteckfläche
+# Lambda-Funktionen für die Flächenberechnung
+circle_area = lambda r: pi * r * r  # Kreisfläche
+square_area = lambda s: s * s       # Quadratfläche
+rectangle_area = lambda l, w: l * w # Rechteckfläche
 
 if __name__ == '__main__':
-
     print(circle_area(5))
     print(square_area(4))
     print(rectangle_area(3, 5))
